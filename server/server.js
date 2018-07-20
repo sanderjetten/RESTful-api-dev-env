0e89fc4fcb567b6dc4844e0a9617e883133226af
Sander Jetten <sanderjetten@gmail.com>
2018-07-20 23:39:43 +0200

    added custom middleware

--- server/server.js
+++ server/server.js
@@ -2,66 +2,69 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var _ = require('lodash');
-
-// express.static will serve everything
-// with in client as a static resource
-// also, it will server the index.html on the
-// root of that directory on a GET to '/'
-app.use(express.static('client'));
-
-// body parser makes it possible to post JSON to the server
-// we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+var morgan = require('morgan');
 
 var lions = [];
 var id = 0;
 
-// TODO: make the REST routes to perform CRUD on lions
-app.get('/lions', function(req, res){
-  if(req.query.name){
-    res.json(_.find(lions, {name: req.query.name}));
-  } else{
-    res.json(lions);
+var updateId = function(req, res, next) {
+  if(!req.body.id){
+    req.body.id = id++;
   }
+  next();
+};
+
+app.use(morgan('dev'))
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.param('id', function(req, res, next, id) {
+  var lion = _.find(lions, {id: parseInt(id)});
+  if(!lion){
+    res.status(404).send('lion not found!');
+  } else{
+    req.lion = lion;
+    next();
+  }
+});
+
+app.get('/lions', function(req, res){
+  res.json(lions);
 });
 
 app.get('/lions/:id', function(req, res){
-  var lion = _.find(lions, {id:parseInt(req.params.id)});
+  var lion = req.lion;
   res.json(lion || {});
 });
 
-app.post('/lions', function(req, res){
+app.post('/lions', updateId, function(req, res) {
   var lion = req.body;
-  lion.id = id++;
+
   lions.push(lion);
+
   res.json(lion);
 });
 
-app.put('/lions/:id', function(req, res){
-  var newLion = req.body;
-  if(newLion.id){
-    delete newLion.id;
+
+app.put('/lions/:id', function(req, res) {
+  var update = req.body;
+  if (update.id) {
+    delete update.id
   }
 
-  var oldLionIndex = _.findIndex(lions, {id: parseInt(req.params.id)});
-  if(!lions[oldLionIndex]){
-    res.status(404).send('Lion not found!');
-  } else{
-    newLion = _.assign(lions[oldLionIndex], newLion);
-    res.json(newLion);
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if (!lions[lion]) {
+    res.send();
+  } else {
+    var updatedLion = _.assign(lions[lion], update);
+    res.json(updatedLion);
   }
 });
 
-app.delete('/lions/:id', function(req, res){
-  var lionIndex = _.findIndex(lions, {id: parseInt(req.params.id)});
-  
-  if(!lions[lionIndex]){
-    res.status(404).send('Lion not found!');
-  } else{
-    var deletedLion = lions[lionIndex];
-    lions.splice(lionIndex, 1);
-    res.json(deletedLion);
+app.use(function(error, req, res, next){ /* eslint-disable-line no-unused-vars */
+  if(error){
+    res.status(500).send(error);
   }
 });
 
