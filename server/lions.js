f4a2478a23d54521c72f98a7de45dd276463d740
Sander Jetten <sanderjetten@gmail.com>
2018-07-25 20:44:14 +0200

    added subrouters

---
+++ server/lions.js
@@ -0,0 +1,63 @@
+var lionRouter = require('express').Router();
+var _ = require('lodash');
+
+var lions = [];
+var id = 0;
+
+var updateId = function(req, res, next) {
+  if (!req.body.id) {
+    id++;
+    req.body.id = id + '';
+  }
+  next();
+};
+
+lionRouter.param('id', function(req, res, next, id) {
+  var lion = _.find(lions, {id: id});
+
+  if (lion) {
+    req.lion = lion;
+    next();
+  } else {
+    res.send();
+  }
+});
+
+lionRouter.route('/')
+  .get(function(req, res){
+    res.json(lions);
+  })
+  .post(updateId, function(req, res) {
+    var lion = req.body;
+  
+    lions.push(lion);
+  
+    res.json(lion);
+  }
+);
+
+lionRouter.route('/:id')
+  .get(function(req, res){
+    var lion = req.lion;
+    res.json(lion || {});
+  })
+  .post(function(req, res) {
+    var update = req.body;
+    if (update.id) {
+      delete update.id
+    }
+  
+    var lion = _.findIndex(lions, {id: req.params.id});
+    if (!lions[lion]) {
+      res.send();
+    } else {
+      var updatedLion = _.assign(lions[lion], update);
+      res.json(updatedLion);
+    }
+  })
+  .delete(function(req, res){ /* eslint-disable-line no-unused-vars */
+    var lion = _.findIndex(lions, {id: req.params.id});
+    lions.splice(lion, 1);
+  });
+
+module.exports = lionRouter;
--- server/server.js
+++ server/server.js
@@ -1,70 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var _ = require('lodash');
 var morgan = require('morgan');
-
-var lions = [];
-var id = 0;
-
-var updateId = function(req, res, next) {
-  if(!req.body.id){
-    req.body.id = id++;
-  }
-  next();
-};
+var lionRouter = require('./lions');
+var tigerRouter = require('./tigers');
 
 app.use(morgan('dev'))
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.param('id', function(req, res, next, id) {
-  var lion = _.find(lions, {id: parseInt(id)});
-  if(!lion){
-    res.status(404).send('lion not found!');
-  } else{
-    req.lion = lion;
-    next();
-  }
-});
+app.use('/lions', lionRouter);
+app.use('/tigers', tigerRouter);
 
-app.get('/lions', function(req, res){
-  res.json(lions);
-});
-
-app.get('/lions/:id', function(req, res){
-  var lion = req.lion;
-  res.json(lion || {});
-});
-
-app.post('/lions', updateId, function(req, res) {
-  var lion = req.body;
-
-  lions.push(lion);
-
-  res.json(lion);
-});
-
-
-app.put('/lions/:id', function(req, res) {
-  var update = req.body;
-  if (update.id) {
-    delete update.id
-  }
-
-  var lion = _.findIndex(lions, {id: req.params.id});
-  if (!lions[lion]) {
-    res.send();
-  } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
-  }
-});
-
-app.use(function(error, req, res, next){ /* eslint-disable-line no-unused-vars */
-  if(error){
-    res.status(500).send(error);
+app.use(function(err, req, res, next){ /* eslint-disable-line no-unused-vars */
+  if(err){
+    console.log(err.message); /* eslint-disable-line no-console */
+    res.status(500).send(err);
   }
 });
 
