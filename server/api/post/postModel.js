60f351696cdd862cda4e0ef7136911157af4a1b1
Sander Jetten <sanderjetten@gmail.com>
2018-08-02 21:41:50 +0200

    Added schemas to resources

---
+++ server/api/post/postModel.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  categories: [{
+    type: Schema.Types.ObjectId,
+    ref: 'category'
+  }]
+});
+
+module.exports = mongoose.model('post', PostSchema);
